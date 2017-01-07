c40a48062e2371962f8621abf8de0563dde80c0f
Justin Johnson <justinaugustejohnson@gmail.com>
2017-01-07 16:33:34 -0500

    app stability edits

--- web/views/name_helper.ex
+++ web/views/name_helper.ex
@@ -3,9 +3,9 @@ defmodule HelloPhoenix.NameHelpers do
 list = ["Ruby", "Rails", "Phoenix", "Elixir", "Unicorny", "Socket", "Pipe", "Random", "Tuple", "Refuctoring"]
 
 def randomName do
-  list |> Enum.shuffle |> Enum.take(2)
+  # list |> Enum.shuffle |> Enum.take(2)
 end
 
-randomName()
+# randomName()
 
 end

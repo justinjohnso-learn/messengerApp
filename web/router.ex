0d08db9fb11d036a46f166b4b17050745ae0e402
Michelle Brant <michellebrant>
2017-01-07 23:08:11 -0500

    add login functionality

--- web/router.ex
+++ web/router.ex
@@ -18,6 +18,9 @@ defmodule HelloPhoenix.Router do
 
     get "/", PageController, :index
     resources "/registrations", RegistrationController, only: [:new, :create]
+    get    "/login",  SessionController, :new
+    post   "/login",  SessionController, :create
+    delete "/logout", SessionController, :delete
   end
 
   # Other scopes may use custom stacks.

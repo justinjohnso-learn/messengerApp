c1c9f98eca4c7805659fc2b064baa20fb240a3bf
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-01-07 19:21:56 -0500

    still working on randomizer

--- web/views/name_helper.ex
+++ web/views/name_helper.ex
@@ -1,12 +1,4 @@
-defmodule HelloPhoenix.NameHelpers do
 
 list = ["Ruby", "Rails", "Phoenix", "Elixir", "Unicorny", "Socket", "Pipe", "Random", "Tuple", "Refuctoring"]
 
-def randomName do
   list |> Enum.shuffle |> Enum.take(2)
-end
-
-randomName()
-
-
-end

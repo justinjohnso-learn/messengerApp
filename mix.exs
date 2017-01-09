15e2b3b5eb0b3b181fa1fc7d816b141e0b1e168e
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-01-09 10:19:55 -0500

    added phoenix file

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule HelloPhoenix.Mixfile do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
+     {:postgrex, ">= 0.10.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:comeonin, "~> 1.0"},

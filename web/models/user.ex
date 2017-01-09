09417b8e47c977b0ee66da04de8ce6aa4c7bb0ef
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-01-09 11:52:20 -0500

    reran migration

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,25 +1,20 @@
 defmodule HelloPhoenix.User do
   use HelloPhoenix.Web, :model
 
- schema "users" do
-  field :email, :string
-  field :crypted_password, :string
-  field :password, :string, virtual: true
-  timestamps
-end
+  schema "users" do
+    field :email, :string
+    field :crypted_password, :string
 
-
-@required_fields ~w(email password)
-@optional_fields ~w(email crypted_password)
+    timestamps()
+  end
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-def changeset(model, params \\ :empty) do
-  model
-  |> cast(params, @required_fields, @optional_fields)
-  |> unique_constraint(:email)
-  |> validate_format(:email, ~r/@/)
-  |> validate_length(:password, min: 5)
-end
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :crypted_password])
+    |> validate_required([:email, :crypted_password])
+    |> unique_constraint(:email)
+  end
 end

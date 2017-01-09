42fce46a186e25443e5a2242336c1070c6aac937
Justin Johnson <justinaugustejohnson@gmail.com>
2017-01-09 13:22:52 -0500

    Trigger Heroku deploy after enabling collectstatic

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hello_phoenix, HelloPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "ga-chatroom.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "radiant-fortress-11127.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")

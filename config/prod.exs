b70542a3db125a97a12f171eada827c0c36d6795
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-01-09 13:18:55 -0500

    changed prod file

--- config/prod.exs
+++ config/prod.exs
@@ -29,13 +29,9 @@ config :logger, level: :info
     config :hello_phoenix, HelloPhoenix.Repo,
       adapter: Ecto.Adapters.Postgres,
       url: System.get_env("DATABASE_URL"),
-      pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+      pool_size: String.to_integer(System.get_env("POOL_SIZE") || "18"),
       ssl: false
-config :hello_phoenix, HelloPhoenix.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+
 #
 # Where those two env variables return an absolute path to
 # the key and cert in disk or a relative path inside priv,

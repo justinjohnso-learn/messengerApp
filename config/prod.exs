99c064484322952ea404a0642430b5d7356d2d30
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-01-09 13:46:28 -0500

    changed some stuff

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hello_phoenix, HelloPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "radiant-fortress-11127.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "ga-messenger.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
@@ -30,7 +30,7 @@ config :logger, level: :info
       adapter: Ecto.Adapters.Postgres,
       url: System.get_env("DATABASE_URL"),
       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "18"),
-      ssl: false
+      ssl: true
 
 #
 # Where those two env variables return an absolute path to
